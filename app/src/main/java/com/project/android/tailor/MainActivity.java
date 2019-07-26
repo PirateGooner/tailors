d4b5f5bbe386ba7da052ccde0abe765ffb412ecf
LeonWanjau <34864276+LeonWanjau@users.noreply.github.com>
2019-07-26 19:06:07 +0300

    Second Commit

--- app/src/main/java/com/project/android/tailor/MainActivity.java
+++ app/src/main/java/com/project/android/tailor/MainActivity.java
@@ -1,5 +1,6 @@
 package com.project.android.tailor;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
@@ -8,6 +9,15 @@ import androidx.lifecycle.ViewModelProviders;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity implements NavigationHelper, CreateAccountFragment.OnCreateAccountFragmentListener {
 
@@ -29,6 +39,28 @@ public class MainActivity extends AppCompatActivity implements NavigationHelper,
                 // .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_CLOSE)
                 //.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
                 .commit();
+
+
+/*
+    FirebaseFirestore db=FirebaseFirestore.getInstance();
+    Map<String,Object> user=new HashMap<>();
+    user.put("fname","Paul");
+    user.put("lname","P");
+    user.put("password","H");
+
+    db.collection("users")
+            .add(user)
+            .addOnSuccessListener(new OnSuccessListener<DocumentReference>(){
+                public void onSuccess(DocumentReference documentReference){
+                    Log.d("MyApp","DocumentSnapshot added with ID: "+documentReference.getId());
+                }
+            })
+            .addOnFailureListener(new OnFailureListener(){
+                public void onFailure(@NonNull Exception e){
+                    Log.w("MyApp","Error adding document",e);
+                }
+            });
+            */
     }
 
     public void navigateTo(Fragment fragment, boolean addToBackstack) {
